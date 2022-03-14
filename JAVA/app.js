3c6deb9bfbfac3eefe1235df4ab22fe6dbae5b58
Ayush binjola <82152077+ayush-binjola@users.noreply.github.com>
2022-03-14 22:59:04 +0530

    Add files via upload

---
+++ JAVA/app.js
@@ -0,0 +1,55 @@
+let weather={
+    apiKey :"a429eb0137d72c100a8ebb040ba63213",
+    fetchWeather:function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ this.apiKey)
+        .then((response)=>response.json())
+        .then((data)=>this.displayWeather(data));
+    },
+    displayWeather : function(data)
+    {
+        const {name } = data;
+        const {icon,description} = data.weather[0];
+        const {temp,humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        // document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity:"+humidity;
+        document.querySelector(".wind").innerText = "Wind:"+speed;
+        document.querySelector(".temp").innerText = "Temprature:"+temp +"℃";
+
+    },
+    search: function()
+    {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+document.querySelector(".search-button").addEventListener("click",function(){
+ weather.search();
+});
+
+
+
+// let inputcity = document.getElementById('enterCity');
+// inputcity.addEventListener("keyup",function(event){
+// if(event.key ==  "Enter" )
+// {
+//     console.log(event.target.value);
+// }
+// });
+// const api =
+// {
+//     url :"https\\api.openweathermap.org/data/2.5/weather?q=",
+//     key:"a429eb0137d72c100a8ebb040ba63213"
+
+// }
+
+// function getResults(cityname)
+// {
+//     fetch(`${api.url}${cityname}&appid=${$api.key}&units=metrics`)
+//     .then((result) => console.log(result));
+// }   
+
+
+
